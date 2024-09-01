64a9923d668528aa2a23cd49e357f80fc761e7af
Anupam <anupamsingh10001@gmail.com>
2024-09-01 20:49:04 +0530

    Main Commit

---
+++ src/hospital/management/system/Login.java
@@ -0,0 +1,99 @@
+package hospital.management.system;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+
+public class Login extends JFrame implements ActionListener {
+
+    JTextField textField;
+    JPasswordField jPasswordField;
+    JButton b1,b2;
+
+
+    Login(){
+
+        JLabel namelabel = new JLabel("Username");
+        namelabel.setBounds(40,20,100,30);
+        namelabel.setFont(new Font("Tahoma",Font.BOLD,16));
+        namelabel.setForeground(Color.WHITE);
+        add(namelabel);
+
+        JLabel password = new JLabel("Password");
+        password.setBounds(40,70,100,30);
+        password.setFont(new Font("Tahoma",Font.BOLD,16));
+        password.setForeground(Color.WHITE);
+        add(password);
+
+        textField = new JTextField();
+        textField.setBounds(150,20,150,30);
+        textField.setFont(new Font("Tahoma",Font.PLAIN,15));
+        textField.setBackground(new Color(200, 192, 209));
+        add(textField);
+
+        jPasswordField = new JPasswordField();
+        jPasswordField.setBounds(150,70,150,30);
+        jPasswordField.setFont(new Font("Tahoma",Font.PLAIN,15));
+        jPasswordField.setBackground(new Color(200, 192, 209));
+        add(jPasswordField);
+
+        ImageIcon imageIcon = new ImageIcon(ClassLoader.getSystemResource("icon/download.jpeg"));
+        Image i1 = imageIcon.getImage().getScaledInstance(250,220,Image.SCALE_DEFAULT);
+        ImageIcon imageIcon1 = new ImageIcon(i1);
+        JLabel label = new JLabel(imageIcon1);
+        label.setBounds(300,-30,400,300);
+        add(label);
+
+        b1 = new JButton("Login");
+        b1.setBounds(40,140,120,30);
+        b1.setFont(new Font("serif",Font.BOLD,15));
+        b1.addActionListener(this);
+        add(b1);
+
+        b2 = new JButton("Cancel");
+        b2.setBounds(180,140,120,30);
+        b2.setFont(new Font("serif",Font.BOLD,15));
+        b2.addActionListener(this);
+        add(b2);
+
+        getContentPane().setBackground(new Color(66, 135, 245));
+        setSize(750,300);
+        setLocation(400,270);
+        setLayout(null);
+        setVisible(true);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if (e.getSource() == b1){
+            try{
+                conn c = new conn();
+                String user = textField.getText();
+                String Pass = jPasswordField.getText();
+
+                String q = "select * from login where ID = '"+user+"' and PW = '"+Pass+"'";
+                ResultSet resultSet = c.statement.executeQuery(q);
+                 if (resultSet.next()){
+                    new Reception();
+                    setVisible(false);
+                }else{
+                     JOptionPane.showMessageDialog(null,"Invalid");
+                 }
+
+
+
+            }catch (Exception E){
+
+            }
+        }else{
+                System.exit(20);
+        }
+    }
+    public static void main(String[] args) {
+        new Login();
+    }
+
+
+}
